ab9fc2d0ffe534bb30bca08caa54d40a8188dd60
zxING Homich,Andrei (AH GSM Swine) EXTERNAL <andrei.homich.ext@boehringer-ingelheim.com>
2023-07-31 12:03:03 +0200

    add new models

--- models/models.py
+++ models/models.py
@@ -4,6 +4,13 @@ from sqlalchemy import Column, ForeignKey, Integer, MetaData, String, Table
 
 metadata = MetaData()
 
+User = Table(
+    'users',
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("username", String),
+)
+
 ZodiacSign = Table(
     'zodiac_signs',
     metadata,
@@ -14,6 +21,25 @@ ZodiacSign = Table(
 
 HoroscopeData = Table(
     'horoscope_data',
+    metadata,
+    Column("id", String, primary_key=True, default=lambda: str(uuid.uuid4())),
+    Column("date", String),
+    Column("text", String),
+    Column("zodiac_sign_id", Integer, ForeignKey('zodiac_signs.id')),
+)
+
+
+LoveHoroscopeData = Table(
+    'love_horoscope_data',
+    metadata,
+    Column("id", String, primary_key=True, default=lambda: str(uuid.uuid4())),
+    Column("date", String),
+    Column("text", String),
+    Column("zodiac_sign_id", Integer, ForeignKey('zodiac_signs.id')),
+)
+
+FinanceHoroscopeData = Table(
+    'finance_horoscope_data',
     metadata,
     Column("id", String, primary_key=True, default=lambda: str(uuid.uuid4())),
     Column("date", String),
