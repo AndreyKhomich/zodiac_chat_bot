021b2d29f45c013eb2c597ceb680f7d2b431bd69
Andrei Khomich <andrei.khomich@lion-software.com>
2024-09-26 17:36:53 +0200

    Adapt bot tu running in docker

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import asyncio
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -14,20 +15,8 @@ from database.database_manager import fetch_dates, get_horoscope_data, save_user
 from keyboards import inline_kb_full, inline_kb_menu
 from models.models import HoroscopeData, LoveHoroscopeData, FinanceHoroscopeData
 
-month_mapping = {
-    'января': 1,
-    'февраля': 2,
-    'марта': 3,
-    'апреля': 4,
-    'мая': 5,
-    'июня': 6,
-    'июля': 7,
-    'августа': 8,
-    'сентября': 9,
-    'октября': 10,
-    'ноября': 11,
-    'декабря': 12
-}
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 bot_token = os.environ.get('BOT_TOKEN')
 port = os.environ.get('PORT')
@@ -38,12 +27,14 @@ dp = Dispatcher(bot, storage=storage)
 dp.middleware.setup(LoggingMiddleware())
 
 
+
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
     username = message.from_user.username
 
     await save_user(user_id, username)
+    logger.info(f"User {username} ({user_id}) started the bot.")  # Log user start
 
     await bot.send_sticker(user_id, sticker="CAACAgIAAxkBAAEJ1Z1kw08f2y6zz4A7rheTsOh_npqF7gACVB0AAoqR0ElUTMG-FBDOOy8E")
     await message.reply(
@@ -59,6 +50,8 @@ async def process_menu(callback_query: CallbackQuery, state: FSMContext):
     selected_menu_option = callback_query.data.split('_')[1]
     await state.update_data(selected_menu_option=selected_menu_option)
     chat_id = callback_query.from_user.id
+
+    logger.info(f"User {chat_id} selected menu option: {selected_menu_option}")  # Log menu selection
 
     await bot.send_message(
         chat_id=chat_id,
@@ -76,6 +69,7 @@ async def handle_unexpected_messages(message: types.Message):
         await message.reply("Пожалуйста, используйте кнопки для взаимодействия с ботом.")
     else:
         await message.reply("Неизвестная команда. Для начала работы с ботом используйте /start.")
+        logger.warning(f"User {message.from_user.id} sent an unexpected message: {message.text}")  # Log unexpected messages
 
 
 @dp.callback_query_handler(lambda c: c.data.startswith('знак_'), state='zodiac_sign')
@@ -83,6 +77,8 @@ async def process_zodiac_sign(callback_query: CallbackQuery, state: FSMContext):
     zodiac_sign = callback_query.data.split('_')[1]
     await state.update_data(zodiac_sign=zodiac_sign)
     chat_id = callback_query.from_user.id
+
+    logger.info(f"User {chat_id} selected zodiac sign: {zodiac_sign}")  # Log zodiac sign selection
 
     dates = await fetch_dates(zodiac_sign, HoroscopeData)
 
@@ -122,10 +118,11 @@ async def day_of_week(callback_query: CallbackQuery, state: FSMContext):
 
             await show_another_day_keyboard(chat_id)
             await dp.current_state().set_state('another_day_option')
+            logger.info(f"Sent horoscope for {zodiac_sign} on {date} to user {chat_id}.")  # Log horoscope sent
 
-    except (InterfaceError, SQLAlchemyError):
+    except (InterfaceError, SQLAlchemyError) as e:
         await bot.send_message(chat_id=chat_id, text="Ошибка соединения с базой данных. Пожалуйста, попробуйте позже.")
-        logging.exception("An error occurred while querying the database.")
+        logger.exception("An error occurred while querying the database.")  # Log exception details
         await state.finish()
 
 
@@ -133,6 +130,8 @@ async def day_of_week(callback_query: CallbackQuery, state: FSMContext):
 async def another_day_option(callback_query: CallbackQuery, state: FSMContext):
     answer = callback_query.data.split('_')[1]
     chat_id = callback_query.from_user.id
+
+    logger.info(f"User {chat_id} selected another day option: {answer}")  # Log user response to options
 
     if answer == 'да':
         user_data = await state.get_data()
@@ -195,23 +194,12 @@ async def show_another_day_keyboard(chat_id):
 
     await bot.send_message(
         chat_id=chat_id,
-        text="Вы желаете получить предсказание на другой день или выбрать новый знак зодиака?",
+        text="Хотите получить предсказание на другой день?",
         reply_markup=keyboard
     )
 
 
-async def run_bot(dispatcher: Dispatcher):
-    await bot.set_webhook(url=f"https://zodiacbot.herokuapp.com/webhook/{bot_token}", drop_pending_updates=True)
-    await bot.set_webhook(url=None)
-    await dispatcher.start_polling()
-
-
 if __name__ == '__main__':
-    executor.start_webhook(
-        dispatcher=dp,
-        webhook_path=f'/webhook/{bot_token}',
-        on_startup=run_bot,
-        skip_updates=True,
-        host='0.0.0.0',
-        port=port
-    )
+    logger.info("Bot started...")
+    loop = asyncio.get_event_loop()
+    executor.start_polling(dp, skip_updates=True)
